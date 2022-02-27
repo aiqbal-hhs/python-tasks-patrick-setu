b0292d0dab7f6f24f2374b9a3a80c630feb1272c
patrick-setu <79290075+patrick-setu@users.noreply.github.com>
2022-02-28 12:44:41 +1300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+food = [[["pie", "piee"], ["pieee", "pieeee"], ["pieeeee", "pieeeeee"]], [["cake", "cakee"], ["cakeee", "cakeeee"], ["cakeeeee", "cakeeeeeee"]]]
+
+print("this is a 3d list".title())
+print(food[1][2][1])

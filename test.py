4af05583ab154a3b8e24ce5c974495997b21f457
patrick-setu <79290075+patrick-setu@users.noreply.github.com>
2022-03-01 14:27:58 +1300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-food = [[["pie", "piee"], ["pieee", "pieeee"], ["pieeeee", "pieeeeee"]], [["cake", "cakee"], ["cakeee", "cakeeee"], ["cakeeeee", "cakeeeeeee"]]]
+food = [[["pie1", "pie2"], ["pie3", "pie4"], ["pie5", "pie6"]], [["cake1", "cake2"], ["cake3", "cake4"], ["cake5", "cake6"]]]
+w = 1
+printt = ["is"]
 
-print("this is a 3d list".title())
+for i in range(w):
+    printt.append("a")
+    printt.append("list")
+    printt.insert(2, "3d")
+    printt.insert(0, "this")
+
+#prints 'this is a 3d list'
+print(" ".join(printt))
+
+#prints last 'cake' in list
 print(food[1][2][1])
+

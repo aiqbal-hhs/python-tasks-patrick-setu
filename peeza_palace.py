e92bad2d6197e894fa0c7a201b6b7cd90b7ddcc3
patrick-setu <79290075+patrick-setu@users.noreply.github.com>
2022-05-19 13:10:46 +1200

    Update peeza_palace.py

--- peeza_palace.py
+++ peeza_palace.py
@@ -1,9 +1,11 @@
+import time
+from collections import namedtuple
+
 pizzas = {"classic cheese": 8.50, "americano": 8.50, "ham and cheese": 8.50, "classic veggie": 8.50, "garlic and aioli": 8.50, "pepperoni": 8.50, "hawaiian": 8.50, "beef and onion": 13.50, "cheesy garlic": 13.50, "chicken deluxe": 13.50, "peri peri chicken": 13.50, "buffalo chicken": 13.50}
-pizza_toppings = {"extra cheese": 0.5, "ham": 0.5, "olives": 0.5, "pepperoni": 0.5, "onions": 0.5}
+pizza_toppings = {"extra cheese": 0.5, "ham": 0.5, "olives": 0.5, "pepperoni": 0.5, "onions": 0.5, "mushrooms": 0.5}
 user_details = {"Name": "" , "Address": "", "Mobile": ""}
 final_order = {}
-user_toppings = {}
-#user_total =
+user_total = 0
 
 #Displays possible user actions
 def actions():
@@ -15,33 +17,63 @@ def actions():
     '4' to exit\n""")
 
 #Displays the menu
-def view_menu():
-    """Displays the available pizzas and toppings for order"""
-    print("\nThe available pizzas that we offer are: \n{}".format("\n".join(pizzas).title()))
-    print("\nThe available toppings are: \n{}".format("\n".join(pizza_toppings).title()))
+#def list_pizzas():
+    #"""Displays the available pizzas and toppings for order"""
+    #print("\nThe available pizzas that we offer are: \n{}".format("\n".join(pizzas).title()))
+    #print("\nThe available toppings are: \n{}".format("\n".join(pizza_toppings).title()))
 
-#Displays the pizzas
-def all_pizzas():
-    print("\nThe available pizzas that we offer are: \n{}".format("\n".join(pizzas).title()))
+#Displays the pizza menu
+list_pizzas = namedtuple('list_pizzas', ['pizza','price'])
+pizza_choices = []
+pizza_choices.append(list_pizzas('- Classic Cheese', '$8.50'))
+pizza_choices.append(list_pizzas('- Americano', '$8.50'))
+pizza_choices.append(list_pizzas('- Ham And Cheese', '$8.50'))
+pizza_choices.append(list_pizzas('- Classic Veggie', '$8.50'))
+pizza_choices.append(list_pizzas('- Garlic And Aioli', '$8.50'))
+pizza_choices.append(list_pizzas('- Pepperoni', '$8.50'))
+pizza_choices.append(list_pizzas('- Hawaiian', '$8.50'))
+pizza_choices.append(list_pizzas('- Beef And Onion', '$13.50'))
+pizza_choices.append(list_pizzas('- Cheesy Garlic', '$13.50'))
+pizza_choices.append(list_pizzas('- Chicken Deluxe', '$13.50'))
+pizza_choices.append(list_pizzas('- Peri Peri Chicken', '$13.50'))
+pizza_choices.append(list_pizzas('- Buffalo Chicken', '$13.50\n'))
 
-#Displays the toppings
-def all_toppings():
-    print("\nThe available toppings are: \n{}".format("\n".join(pizza_toppings).title()))
+def view_pizzas():
+    for entry in pizza_choices:
+        pizza = getattr(entry,'pizza').ljust(25)
+        price = getattr(entry,'price').ljust(7)
+        print('{}{}'.format(pizza,price))
+
+#Display toppings Menu
+list_toppings = namedtuple('list_toppings', ['topping','price'])
+topping_choices = []
+topping_choices.append(list_toppings('Extra Cheese', '$0.50'))
+topping_choices.append(list_toppings('Extra Ham', '$0.50'))
+topping_choices.append(list_toppings('Extra Olives', '$0.50'))
+topping_choices.append(list_toppings('Extra Pepperoni', '$0.50'))
+topping_choices.append(list_toppings('Extra Onions', '$0.50'))
+topping_choices.append(list_toppings('Extra Mushrooms', '$0.50'))
+
+def view_toppings():
+    for entry in topping_choices:
+        topping = getattr(entry,'topping').ljust(25)
+        price = getattr(entry,'price').ljust(7)
+        print('{}{}'.format(topping,price))
 
 #Create function for ordering pizza
 def service_type():
     """Displays the options of service for the user, or to go back to menu"""
     repeat = True
     while repeat:
-        print("\nFor delivery, enter '1'. For pick-up, enter '2'. To go back to menu, enter '3'")
+        print("\nFor delivery($3.00 surcharge), enter '1'. For pick-up, enter '2'. To go back to menu, enter '3'")
         service = input("What would you like to do?\n")
         if service == "1":
-            print("You chose delivery.")
+            print("\nYou chose delivery.")
             repeat = True
             while repeat:
                 address = input("What is the address?\n").strip().lower()
                 while repeat:    
-                    confirm = input("The address is {}. Is this correct? Enter 'yes' or 'no'\n".format(address.title())).strip().lower()
+                    confirm = input("\nThe address is {}. Is this correct? Enter 'yes' or 'no'\n".format(address.title())).strip().lower()
                     if confirm == "yes":
                         user_details["Address"] = address
                         repeat = False
@@ -52,9 +84,9 @@ def service_type():
                         continue
             repeat = True
             while repeat:
-                mobile = input("What mobile number should we contact you with?\n").strip()
+                mobile = input("\nWhat mobile number should we contact you with?\n").strip()
                 while repeat:    
-                    confirm = input("The mobile number you entered is {}. Is this correct? Enter 'yes' or 'no'\n".format(mobile)).strip()
+                    confirm = input("\nThe mobile number you entered is {}. Is this correct? Enter 'yes' or 'no'\n".format(mobile)).strip()
                     if confirm == "yes":
                         user_details["Mobile"] = mobile
                         repeat = False
@@ -63,42 +95,45 @@ def service_type():
                     else:
                         print("\nWarning: Please enter 'yes' or 'no'\n")
                         continue
-            order(final_order, user_toppings)
-
+            order(final_order, user_total)
+            break
         elif service == "2":
             print("\nYou chose pick-up.")
             print("You can order up to 5 pizzas from our available pizza range")
-            #order function
+            order(final_order, user_total)
+            break
         elif service == "3":
-            actions()
+            break
         else:
             print("\nWarning: Please enter '1', '2', or '3'\n")
             continue
 
 #Orders the user's pizza/s
-def order(pizza, topping):
-    """"User selects pizzas and toppings then displays order"""
-    all_pizzas()
+def order(pizza, user_total):
+    """"User selects a pizza and toppings(optional) then displays order"""
+    view_pizzas()
     ordering_pizza = True
     while ordering_pizza:
         pizza = input("What pizza would you like to select?\n").strip().lower()
         if pizza in pizzas:
-            print("You have selected the {} pizza".format(pizza))
+            print("\nYou have selected the {} pizza".format(pizza))
+            user_total += pizzas.get(pizza)
+            print(pizzas.get(pizza))
             ordering_pizza = False
         else:
             print("Sorry, we do not have {} pizza".format(pizza))
     ordering_topping = True
     while ordering_topping:
-        add_topping = input("Would you like to add a topping? Enter 'yes' or 'no'\n").strip().lower()
-        #fix add topping loop
+        add_topping = input("Would you like to add more toppings? Enter 'yes' or 'no'\n").strip().lower()
         if add_topping == "yes":
             repeat = True
             while repeat:
-                view_toppings = input("Do you want to view the available toppings? Please enter 'yes' or 'no'\n").strip().lower()
-                if view_toppings == "yes":
-                    all_toppings()
+                list_toppings = input("Do you want to view the available toppings? Please enter 'yes' or 'no'\n").strip().lower()
+                if list_toppings == "yes":
+                    print("")
+                    view_toppings()
                     repeat = False
-                elif view_toppings == "no":
+                elif list_toppings == "no":
                     repeat = False
                 else:
                     print("\nWarning: Please enter 'yes' or 'no'\n")
@@ -112,15 +147,23 @@ def order(pizza, topping):
         if topping in pizza_toppings:
             final_order.setdefault(pizza, [])
             final_order[pizza].append(topping)
-            print("I have added {} to your {}".format(topping, pizza))
-            print(final_order)
-            #for key, value in final_order.items():
-                #print("You have ordered a {} with {}".format(pizza, topping))
+            user_total += pizza_toppings.get(topping)
+            if len(final_order[pizza]) > 1:
+                print(final_order)
+                print(user_total)
+                print(len(final_order[pizza]))
+                #print out the multiple toppings in one print statement
+                #print {pizza} with {""}
+
+            else:
+                print("I have added {} to your {} pizza".format(topping, pizza))
+                print(user_total)
+                for key, value in final_order.items():
+                    print("You have ordered a {} with {}".format(pizza, topping))
         else:
             print("Sorry, we do not have {}\n".format(topping))
-        
-#fix toppings and pizza order func
-
+            
+    
 #---------------------------------------------------------------------------------------------
 
 #Collects user's name and greets them
@@ -136,9 +179,14 @@ while repeat:
     if user_action == "1":
         continue
     elif user_action == "2":
-        view_menu()
+        print("")
+        view_pizzas()
+        view_toppings()
     elif user_action == "3":
         service_type()
+        print("Your order will be ready soon. Thank you for calling Henderon High School Pizza Palace!")
+        print(user_total)
+        break
     elif user_action == "4":
         print("\nThank you for calling Henderon High School Pizza Palace.")
         print("\nFarewell {}!".format(user_details["Name"]))
@@ -146,7 +194,10 @@ while repeat:
     else:
         print("\nWarning: Please enter one of the available actions.")
         continue
-#Fix else repeat loop
 
 
-#Create function for adding total
+#to-do list:
+#add $3 charge for delivery
+#add costs to global user_total
+#fix spacings
+#create if else for dictionary in list using index greater than 0, display pizza with more than 1 topping 
